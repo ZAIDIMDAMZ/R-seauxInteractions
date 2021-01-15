ba8e39652fc48afded460b5096f6da257503d634
Gudin <thibaut.gudin@free.fr>
2021-01-15 14:38:30 +0100

    Debut programme

--- Grand_Reseaux_Interaction_TP1/src/gri2021/tp1/Graph.java
+++ Grand_Reseaux_Interaction_TP1/src/gri2021/tp1/Graph.java
@@ -2,4 +2,20 @@ package gri2021.tp1;
 
 public class Graph {
 	int nbS, nbA;//nb sommets et arrêtes
+	int[] sommets;
+	int[] ls_adja[];
+	
+	public Graph(int nbS) {
+		this.nbS = nbS;
+		sommets = new int[nbS];
+		ls_adja = new int[nbS][];
+		for(int i = 0;i<nbS;i++) {
+			ls_adja[i] = new int[nbS];
+		}
+	}
+	
+	public void neighbors() {
+		//TODO
+	}
+	
 }
