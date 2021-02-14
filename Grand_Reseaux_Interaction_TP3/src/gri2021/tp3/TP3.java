3305df58714fe4f9c8a14ca4f49adbe3fb2adb3d
Gudin <thibaut.gudin@free.fr>
2021-02-14 16:34:03 +0100

    Clustering local global + nombre de triangle totals dans le graph

--- Grand_Reseaux_Interaction_TP3/src/gri2021/tp3/TP3.java
+++ Grand_Reseaux_Interaction_TP3/src/gri2021/tp3/TP3.java
@@ -35,50 +35,68 @@ public class TP3 {
 		return s;
 	}
 	*/
-
+	
+	//Indique si l'argument donné est connu ou non de l'exécution
+	private static boolean action_connue(String arg) {
+		if(arg.compareTo("triangles") == 0 || arg.compareTo("clust") == 0) {
+			return true;
+		}
+		return false;
+	}
+	
 	//triangles test.txt 7 1 ; triangles as-caida20071105-simple.txt 53381 123
+	// clust test.txt 7 ;
 	public static void main(String[] args) throws Exception {
 		//Le fichier doit être lu avant de construire le graph
 		String action = args[0];
 		String fichier = args[1];
 		int estim = Integer.parseInt(args[2]);//Une estimation du nbr d'arcs
-		int u = Integer.parseInt(args[3]);//Le point de départ du parcours
 		if(estim <= 0) {
 			throw new Exception("Le paramètre 'estimation du nombre d'arcs' est erroné");
 		}
+		if(!action_connue(action)) {
+			throw new Exception("L'action <<"+action+">> est inconnue du programme!");
+		}
 		Lecteur_Fichier Reader = new Lecteur_Fichier(fichier, estim);
 		//System.out.println("action = "+action+", fichier = "+fichier +", arg1 = "+estim+" et on part du point "+u);
+		//System.out.println("Lecture du graph en cours:");
+		Reader.Read();
+		//System.out.println("Lecture terminée:");
+		//mem();
+		//On génère un Graph pour mieux stocker les données qui viennent d'être lues
+		Graph g = new Graph(Reader, true);
+		Voisinage voisi = new Voisinage(g.nbS);//Étude du voisinage des sommets entre eux
 		if(action.compareTo("triangles") == 0) {
-			//System.out.println("Lecture du graph en cours:");
-			Reader.Read();
-			//System.out.println("Lecture terminée:");
-			//mem();
-			//On génère un Graph pour mieux stocker les données qui viennent d'être lues
-			Graph g = new Graph(Reader, true);
-			Voisinage voisi = new Voisinage(g.nbS);
+			int u = Integer.parseInt(args[3]);//Le point de départ du parcours
 			System.out.println(""+voisi.nb_triangles(u, g));
-			
 		}else if(action.compareTo("clust") == 0) {
-			//System.out.println("Lecture du graph en cours:");
-			Reader.Read();
-			//System.out.println("Lecture terminée:");
-			//mem();
-			//On génère un Graph pour mieux stocker les données qui viennent d'être lues
-			Graph g = new Graph(Reader, true);
-			Voisinage voisi = new Voisinage(g.nbS);
 			//double [] cluL = new double[g.get_nbS()];//Le clustering local de cahque sommet
 			double cluL = 0.0;
 			
 			//Clustering local de chaque sommet:
 			for(int i = 0;i<g.get_nbS();i++) {
-				int tri = voisi.nb_triangles(u, g);
-				cluL += (double) ((2*tri)/(g.get_deg(u)*(g.get_deg(u) - 1)));
-				
+				int tri = voisi.nb_triangles(i, g);
+				//System.out.println("      Le sommet "+i+" est contenu dans "+tri+" triangles");
+				if(g.get_deg(i)*(g.get_deg(i) - 1) > 0){
+					//System.out.println("      deg("+i+") = "+g.get_deg(i));
+					//Peut être utilisé à la place du "+=" de cluL si on souhaite débuger
+					/*
+					double cluLx = (double) (2*tri)/(g.get_deg(i)*(g.get_deg(i)-1));//cluL devient la somme des clustering locaux	
+					System.out.println("      cluL("+i+") = "+cluLx);
+					cluL += cluLx;*/
+					
+					cluL += (double) (2*tri)/(g.get_deg(i)*(g.get_deg(i)-1));//cluL devient la somme des clustering locaux
+				}else {
+					cluL += (double) (2*tri);
+				}
 			}
-			cluL = cluL/g.get_nbS();
-			//Clustering local moyen:			
+			cluL = cluL/g.get_nbS();//Calul du clustering local moyen
+			//Clustering local moyen:
+			int triG = voisi.nb_triangles_G(g);
+			System.out.println("Le Graph contient: "+triG+" triangles au total");
 			//TODO: fonction qui calcul nv = le nombre de "2 arêtes incidentes" du graphe
 			//double cluG = (3 * triG)/(nv);//TODO triG = nombre de triangles totales dans le graphe
+			System.out.format("%.5f\n", cluL);
 		}else {
 			throw new Exception("L'action <<"+action+">> est inconnue du programme!");
 		}
--- Grand_Reseaux_Interaction_TP3/src/gri2021/tp3/Voisinage.java
+++ Grand_Reseaux_Interaction_TP3/src/gri2021/tp3/Voisinage.java
@@ -4,27 +4,34 @@ import java.util.Iterator;
 
 //Cette fonction sert à vérifier quels sont les voisins d'un sommet
 public class Voisinage {
-	boolean[] is_v/*, deja_vu*/;
+	boolean[] is_v, deja_vu;
 	boolean done = false;//Indique si la fonction a déjà été effectuée
 	int triangles = 0;//Le nombre de triangles
 	
 	Voisinage(int n){
 		is_v = new boolean[n];
+		deja_vu = new boolean[n];
 		//deja_vu = new boolean[n];
 		for(int i = 0;i<n;i++) {
 			is_v[i] = false;
-			//deja_vu[i] = false;
+			deja_vu[i] = false;
 		}
 	}
 	
 
-	//???
-	public void clear() {
+	//réinitialise le tableau à faux
+	private void clear() {
 		for(int i=0; i < is_v.length;i++) {
 			is_v[i] = false;
 		}
 		triangles = 0;
 		done = false;
+	}
+	
+	private void clear_deja_vu() {
+		for(int i = 0; i < deja_vu.length;i++) {
+			deja_vu[i] = false;
+		}
 	}
 	
 	private boolean are_voisins(int x, int y, Graph G) {
@@ -43,31 +50,52 @@ public class Voisinage {
 	}
 	
 	public int nb_triangles(int u, Graph G) {
-		if(!done) {
-			//??? Je ne comprend
-			Iterator<Integer> ls_v = G.neighbors(u).iterator();
-			while(ls_v.hasNext()) {
-				is_v[ls_v.next()] = true;
+		if(done) {
+			clear();//On réinitialise le tableau si nécessair
+		}
+		//??? Es-ce qui était attendu?
+		Iterator<Integer> ls_v = G.neighbors(u).iterator();
+		while(ls_v.hasNext()) {
+			int x = ls_v.next();//On pioche le nouveau voisin de l'itérateur
+			//Si un sommet est "deja_vu" inutile de le considérer comme un voisin
+			if(!deja_vu[x]) {
+				is_v[x] = true;
 			}
-			//???
-			
-			//On indique quels sont les sommets voisins du sommet u
-			for(int i = 0; i < is_v.length;i++) {
-				//Si un sommet est voisin de u et n'a pas encore été vu
-				if(is_v[i]) {
-					for(int j = i+1;j < is_v.length;j++) {
-						if(is_v[j]) {
-							//System.out.println(""+i+" et "+j+" sont ils voisins?");
-							if(are_voisins(i, j, G) /*&& !deja_vu[j]*/) {
+		}
+		//???
+		//On cherche un sommet voisin de u
+		for(int i = 0; i < is_v.length;i++) {
+			//Si un sommet est voisin de u
+			if(is_v[i]) {
+				//On regarde parmi les sommets restants
+				for(int j = i+1;j < is_v.length;j++) {
+					//Si est un autre voisin de u
+					if(is_v[j]) {
+						//System.out.println(""+i+" et "+j+" sont ils voisins?");
+						if(!deja_vu[j]) {
+							//On Vérifie si les 2 voisins de u sont voisins entre eux
+							if(are_voisins(i, j, G)) {
 								triangles++;
 							}
 						}
 					}
-					//deja_vu[i] = true;
 				}
+				//deja_vu[i] = true;
 			}
-			done = true;
 		}
+		done = true;
 		return triangles;
+	}
+	
+	//Retourn le nombre total de triangles contenus dans le Graph
+	public int nb_triangles_G(Graph G) {
+		int x = 0;
+		for(int i = 0;i < is_v.length;i++) {
+			x += nb_triangles(i, G);//Combiens de triangles touchent le sommet "i"
+			deja_vu[i] = true;//On ne veux plus jamais que "i" soi repris en compte pour les autres triangles
+		}
+		clear_deja_vu();
+		//On a le nombre de triangles du graph
+		return x;
 	}
 }
