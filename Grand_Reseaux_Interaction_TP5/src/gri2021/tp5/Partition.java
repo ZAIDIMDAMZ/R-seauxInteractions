c3a64e550e2b0238a2ec0d81d01705aba9e04617
Gudin <thibaut.gudin@free.fr>
2021-03-13 16:08:12 +0100

    Correction exercice 1 delta

--- Grand_Reseaux_Interaction_TP5/src/gri2021/tp5/Partition.java
+++ Grand_Reseaux_Interaction_TP5/src/gri2021/tp5/Partition.java
@@ -1,38 +1,63 @@
 package gri2021.tp5;
 
+import java.util.Iterator;
+
 public class Partition {
-	Communaute commu[];//Le num de commu associé à chaque sommet
+	int commu[];//Le num de commu associé à chaque sommet
+	int SC[];
 	long modul;//modularite
 	long m;
 	Graph G;
 	
-	public long to_long(int x) {
+	//J'avais mal compirs l'énoncé, cette opération ne convertie pas en long
+	/*public long to_long(int x) {
 		return (1L * x * x);
+	}*/
+	
+	public long get_dC(int x, int C) {
+		int dC = 0;
+		Iterator<Integer> ls_v = G.neighbors(x).iterator();
+		while(ls_v.hasNext()) {
+			int v = ls_v.next();
+			if(commu[v] == C) {
+				dC++;
+			}
+		}
+		return dC;
 	}
 	
-	public long maj_Q(int u, int v) {
-		return ((to_long(4)*m)*(commu[u].get_dC(u) - commu[v].get_dC(v))) - (to_long(2*G.get_deg(u))* (commu[u].getSC() - commu[v].getSC() + to_long(G.get_deg(u))));
+	public long maj_Q(int u, int v, int B) {
+		//return ((to_long(4)*m)*(commu[u].get_dC(u) - commu[v].get_dC(v))) - (to_long(2*G.get_deg(u))* (commu[u].getSC() - commu[v].getSC() + to_long(G.get_deg(u))));
+		return (4*m)*(get_dC(u, commu[v]) - get_dC(v, B)) - (2*G.get_deg(u)* (SC[v] - SC[u] + G.get_deg(u)));
 	}
 	
 	//Deplacer u vers la communaute de v
 	public void deplace_to(int u, int v) {
-		commu[u].supp_s(u);
-		commu[v].add_s(u);
-		modul += maj_Q(u,v);
+		/*commu[u].supp_s(u);
+		commu[v].add_s(u);*/
+		int B = commu[u];
+		commu[u] = commu[v];
+		modul += maj_Q(u,v,B);
+		SC[v] += SC[u];
+		SC[u] = SC[v];
 	}
 	
 	Partition(Graph G){
 		this.G = G;
-		commu = new Communaute[G.get_nbS()];
+		commu = new int[G.get_nbS()];
+		SC = new int[G.get_nbS()];
 		for(int i = 0; i < G.get_nbS(); i++) {
-			commu[i] = new Communaute(i, G);//Chaque sommet commence dans sa propre communauté
+			commu[i] = i;//Chaque sommet commence dans sa propre communauté
+			SC[i] = G.get_deg(i);
 		}
-		m = to_long(G.get_nbA());
+		m = G.get_nbA();
+		System.out.println("m = "+m);
 		//TODO calcul modularite initial
-		modul = to_long(0);
+		modul = 0;
 		for(int i = 0; i < commu.length;i++) {
-			//modul += ((to_long(4)*m)*(to_long(G.get_deg(i)))) - (to_long(2*G.get_deg(i))*(commu[i].getSC()+to_long(G.get_deg(i))));
-			modul += (to_long(2)*m) / (to_long(1) - (commu[i].getSC()^2));
+			//modul += ((2*m)/G.get_deg(i)) - (SC[i]^2);//Tentative échouée
+			//Cf squelette correction, j'ai essayé en vais d'appiquer la formule que m'a montré l'enseignant
+			modul -= SC[i] * SC[i];
 		}
 	}
 	
